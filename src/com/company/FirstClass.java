3e651787e0ef52e352895657cb4b73777c274720
savetka <savetkaantovski22@gmail.com>
2023-01-30 20:38:53 +0100

    my first commit

---
+++ src/com/company/FirstClass.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class FirstClass {
+
+
+    private int age=15;
+
+
+    public static void sayHello(String name){
+        System.out.printf("Hello "+name);
+    }
+}
+
+
---
+++ src/tests/RegistrationFormTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import com.sun.jmx.snmp.internal.SnmpAccessControlModel;
+
+import javax.xml.namespace.QName;
+
+public class RegistrationFormTest {
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.printf("Hello world");
+        sayHello( "Savetka");
+        //TODO: Add somethin new
+    }
+//find something
+    /*
+    next comment
+
+
+     */
+    public int getAge() {
+        return age;
+    }
+
+    private int age=15;
+
+
+    public static void sayHello(String name){
+        System.out.printf("Hello "+name);
+    }
+}
+
+
